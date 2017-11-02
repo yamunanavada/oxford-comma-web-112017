99b35146d131e44c9ed779b607b7767984c52dd3
Yamuna Navada <yamunanavada@gmail.com>
2017-11-02 22:07:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
 
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last = array.pop
+    string = array.join(", ")
+    string += ", and #{last}"
+
+
+  end
 end
