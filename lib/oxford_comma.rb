b3f396a7fce95b2332f76e7541cef2cfcdaa8621
Yamuna Navada <yamunanavada@gmail.com>
2017-11-02 22:03:13 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,5 +4,15 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2
     array.join(" and ")
+  else 
+    arr_end = []
+    2.times do
+      arr_end.unshift(array.pop)
+    end
+
+    string = array.join(", ")
+    string+= arr_end.join(" and ")
+    
+      
   end
 end
